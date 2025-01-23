07d5e6bc26624abeef100d5bbe6863e9ab80520d
JuanShos <juanito.martinon5@gmail.com>
2025-01-23 02:40:06 -0800

    Initial Commit

---
+++ PopcornTheaterTests/PopcornTheaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopcornTheaterTests.swift
+//  PopcornTheaterTests
+//
+//  Created by Juanito Martinon on 1/23/25.
+//
+
+import Testing
+@testable import PopcornTheater
+
+struct PopcornTheaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
