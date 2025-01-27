ea674588f60d7a62c0487d2c451f94e7763741c6
JuanShos <juanito.martinon5@gmail.com>
2025-01-27 13:36:31 -0800

    network calls

--- PopcornTheater/DataCalls/NowPlayingCall.swift
+++ PopcornTheater/DataCalls/NowPlayingCall.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+class NowPlayingCall {
+    
+    let queryItems: [URLQueryItem] = [
+      URLQueryItem(name: "language", value: "en-US"),
+      URLQueryItem(name: "page", value: "1"),
+    ]
+    
+    func nowPlayingCall() async throws {
+        // guard helps if url has no value
+        guard let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing") else {
+            return
+        }
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        //urlComponents?.queryItems = queryItems.map { $0 + queryItems } ?? queryItems
+                 
+    }
+    
+    
+    
+    
+    
+    
+}
+
--- PopcornTheater/ViewController.swift
+++ PopcornTheater/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("hello world!!")
         print("hello from my imac")
+        // MY ACCESS TOKEN
+        print("eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzhmZGI1ODc5MGZhNWI0MTJjNWY4ZjFhYjM4Mzg4OSIsIm5iZiI6MTY5Mjc2MDM1MC41NTUsInN1YiI6IjY0ZTU3OTFlYzYxM2NlMDBjOWYwOGI1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2n4706NH9_3zNdJtYZaoKYq3gg-uhTCxpQeECdhZwW4")
     }
 
 
